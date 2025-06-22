d0d43054f4d250f37ed65c6e9d1041a5b583d838
MukeshAbhi <abhimukesh284@gmail.com>
2025-06-23 00:59:27 +0530

    completed express part

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express"
+import Users from "../db/models/userModel"
+
+export const getUserFriends = async(req: Request, res: Response, next: NextFunction) => {
+    
+    try {
+        //from Middleware 
+        const { userId } = req.body.user;
+
+        const user = await Users.findById(userId).populate("friends");
+
+        if (!user) {
+            res.status(200).json({
+                message: "User Not Found",
+                success: false,
+            });
+            return; 
+        }
+
+        user.password = "";
+
+        res.status(200).json({
+            success: true,
+            user: user
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "auth error",
+            success: false,
+            error: error
+        })
+    }
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { login } from "../controllers/login";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { getUserFriends } from "../controllers/user";
 
 
 export const router = Router();
 
-router.use(`/auth/login`, login)
+router.post(`/auth/login`, login)
+router.get('/user/get-friends', authMiddleware, getUserFriends)
