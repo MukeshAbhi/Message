7e7526f73ffc9fa0d7e58e34099b9b5382a19de2
MukeshAbhi <abhimukesh284@gmail.com>
2025-06-26 02:44:31 +0530

    cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dbConnection from "./db";
 import cors from "cors";
 import 'dotenv/config';
 import errorMiddleware from "./middleware/errorMiddleware";
+import cookieParser from "cookie-parser"
 import { router } from "./routes";
 import { io, app, server } from "./lib/socket";
 
@@ -10,15 +11,7 @@ const port = process.env.PORT || 3001;
 
 dbConnection();
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "https://messenger.mukeshtech.site/" 
-    ],
-    credentials: true
-}));
+app.use(cors());
 
 // Increase payload limits for image uploads
 app.use(express.json({ limit: '50mb' }));
